ed04508ed49225c6b62bd21da753277463786a25
FriendlyFire0725 <wichaya002@gmail.com>
2023-04-01 15:37:34 +0700

    log in v1
    WIP need to clean up files location and connect to database

--- app.js
+++ app.js
@@ -1,22 +1,86 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
-const app = express()
+const app = express();
 const cors = require("cors");
 const AppError = require("./utils/appError");
 const errorHandler = require("./utils/errorHandler");
+//for login
+const bcrypt = require("bcrypt"); //yarn add bcrypt
+const passport = require("passport");
+const initializePassport = require("./passport-config");
+const flash = require("express-flash");
+const session = require("express-session");
 
-app.use(express.json())
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 
+const users = [];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, //We wont resave the session vairable if nothing is changed
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/user", //route to home page
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+//configureing the register post functionality
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    console.log(users);
+    res.redirect("/login");
+  } catch (e) {
+    console.log(e);
+    res.redirect("/register");
+  }
+});
+
+//Routes
 const userRoute = require("./routes/user");
 app.use("/user", userRoute);
 
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
 app.all("*", (req, res, next) => {
- next(new AppError(`The URL ${req.originalUrl} does not exists`, 404));
+  next(new AppError(`The URL ${req.originalUrl} does not exists`, 404));
 });
 app.use(errorHandler);
 
 const PORT = 8080;
 app.listen(PORT, () => {
- console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
