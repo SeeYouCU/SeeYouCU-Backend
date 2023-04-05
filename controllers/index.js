9a517e256233f897b809e5fbc3dc8c85bf1f3e47
Wirachapong.suw <73430548+wirachapong@users.noreply.github.com>
2023-04-05 11:02:25 +0700

    addnewfunction: addnewtagtoolduser

--- controllers/index.js
+++ controllers/index.js
@@ -24,6 +24,7 @@ exports.getUser = (req, res, next) => {
 }
 
 exports.getTag= (req, res, next) => {
+  console.log("startgetTag");
   if (!req.params.id) {
     return next(new AppError("No user id found", 404));
   }
@@ -34,6 +35,7 @@ exports.getTag= (req, res, next) => {
     });
   });
 }
+
 
 exports.createUser= (req, res, next) => {
   conn.query(
@@ -48,3 +50,39 @@ exports.createUser= (req, res, next) => {
     }
   );
 }
+
+exports.addTag = (req, res, next) => {
+  // ... no changes in the first part of the function
+
+  conn.query("SELECT tags FROM user WHERE id = ?", [req.params.id], function (err, data, fields) {
+    if (err) return next(new AppError(err, 500));
+
+    if (data.length === 0) {
+      return next(new AppError("User not found", 404));
+    }
+
+    const userTags = data[0].tags;
+
+    if (userTags.includes(req.query.tag)) {
+      return res.status(200).json({
+        status: "success",
+        message: "Tag already exists for the user.",
+      });
+    }
+
+    userTags.push(req.query.tag);
+    const updatedTags = JSON.stringify(userTags);
+
+    conn.query("UPDATE user SET tags = ? WHERE id = ?", [updatedTags, req.params.id], function (err, result) {
+      if (err) return next(new AppError(err, 500));
+
+      res.status(200).json({
+        status: "success",
+        message: "Tag added successfully",
+      });
+    });
+  });
+};
+
+
+
