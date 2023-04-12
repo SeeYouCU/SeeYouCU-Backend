41e7193b6c398f1322384bb59bd3e2ee156480aa
ployloyloy <ployloyloy06@gmail.com>
2023-04-12 23:35:52 +0700

    unfriend

--- controllers/friend.js
+++ controllers/friend.js
@@ -6,8 +6,15 @@ export const makeFriendWith = (req, res) => {
   const token = req.cookies.accessToken;
   jwt.verify(token, "secretkey", (err, userInfo) => {
     if (err) return res.status(403).json("Token is not valid!");
+
+    // const k = "SELECT * FROM isFriendWith WHERE followerUserId = ?";
+    // db.query(k, [userInfo.id], (err, data) => {
+    //   if (err) return res.status(500).json(err);
+    //   if (data.length) return res.status(409).json("User already be friend");
+    // });
+
     const q =
-      "INSERT INTO isFriendWith (`followerUserId`, `followedUserId`, `date`) VALUE (?)";
+      "INSERT IGNORE INTO isFriendWith (`followerUserId`, `followedUserId`, `date`) VALUE (?)";
     const values = [
       userInfo.id,
       req.body.followedUserId,
@@ -20,12 +27,26 @@ export const makeFriendWith = (req, res) => {
   });
 };
 
+export const unFriendWith = (req, res) => {
+  const token = req.cookies.accessToken;
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q = "DELETE FROM isFriendWith WHERE id = ?";
+    const values = [req.body.id];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("user are unfriend!");
+    });
+  });
+};
+
 export const isFriendWith = (req, res) => {
   const token = req.cookies.accessToken;
   jwt.verify(token, "secretkey", (err, userInfo) => {
     if (err) return res.status(403).json("Token is not valid!");
     const q = `SELECT i.id AS RelationshipId, u.id AS userId, followedUserId AS friendWith, date FROM isFriendWith AS i LEFT JOIN user AS u ON (i.followerUserId = u.id) 
-    WHERE i.followerUserId = ? OR i.followedUserId = ? ORDER BY i.date DESC`;
+    WHERE i.followerUserId = ? ORDER BY i.date DESC`;
     db.query(q, [userInfo.id, userInfo.id], (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json(data);
--- routes/friend.js
+++ routes/friend.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { makeFriendWith, isFriendWith } from "../controllers/friend.js";
+import {
+  makeFriendWith,
+  unFriendWith,
+  isFriendWith,
+} from "../controllers/friend.js";
 
 const router = express.Router();
 
 router.post("/makeFriendWith", makeFriendWith);
+router.post("/unFriendWith", unFriendWith);
 router.get("/isFriendWith", isFriendWith);
 
 export default router;
