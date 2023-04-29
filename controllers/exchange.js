47bf8735f812f672881fd11610426ff612b3a11d
ployloyloy <ployloyloy06@gmail.com>
2023-04-29 14:16:15 +0700

    code conflict fix

--- controllers/exchange.js
+++ controllers/exchange.js
@@ -46,7 +46,6 @@ export const getItemState = (req, res) => {
   });
 };
 
-
 export const getExchangeItems = (req, res) => {
   const token = req.cookies.accessToken;
   jwt.verify(token, "secretkey", (err, userInfo) => {
@@ -54,18 +53,6 @@ export const getExchangeItems = (req, res) => {
     const q = `SELECT i.*, u.id AS userId, name, profilePic FROM Exchange AS i LEFT JOIN user AS u ON (i.userId = u.id) 
     WHERE i.accountID = ? OR i.userid = ? ORDER BY i.dateExchange DESC`;
     db.query(q, [userInfo.id, userInfo.id], (err, data) => {
-      if (err) return res.status(500).json(err);
-      return res.status(200).json(data);
-    });
-  });
-};
-
-export const getItemState = (req, res) => {
-  const token = req.cookies.accessToken;
-  jwt.verify(token, "secretkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
-    const q = `SELECT i.state FROM Exchange AS i WHERE i.itemID = ? AND i.accountID = ?`;
-    db.query(q, [req.body.itemID, userInfo.id], (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json(data);
     });
